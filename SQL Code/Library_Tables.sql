57c1ad5a6bf576e03a94ca5c76e70f462d1843a7
Kolbe Williams-Wimmer <kolbeleowilliams@gmail.com>
2025-04-11 22:10:25 -0500

    Add files via upload

---
+++ SQL Code/Library_Tables.sql
@@ -0,0 +1,85 @@
+﻿USE Library
+
+--Employee Table
+CREATE TABLE Employee(
+	EID INT IDENTITY(1,1),
+	first_name VARCHAR(15),
+	last_name VARCHAR(15),
+	phone_number VARCHAR(15) NOT NULL UNIQUE,
+	date_of_birth VARCHAR(10),
+	SUID INT,
+	PRIMARY KEY (EID),
+	CONSTRAINT FK_SUID_Employee
+	FOREIGN KEY (SUID) REFERENCES Employee(EID)
+);
+
+--Section Table
+CREATE TABLE Section(
+	SID INT IDENTITY(1,1),
+	SUID INT,
+	genre VARCHAR(30),
+	PRIMARY KEY (SID),
+	CONSTRAINT FK_EID_Section
+	FOREIGN KEY (SUID) REFERENCES Employee(EID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	CONSTRAINT Check_genre
+		CHECK(genre IN('Science Fiction', 'Fantasy', 'Mystery', 'Romance', 'Thriller', 
+			'Historical Fiction', 'Horror', 'Non-fiction', 'Young Adult', 'Childrens Literature'))
+);
+
+--Customer Table
+CREATE TABLE Customer(
+	CID INT IDENTITY(1,1),
+	EID INT,
+	total DECIMAL(15,2),
+	PRIMARY KEY (CID),
+	CONSTRAINT FK_EID_Customer
+	FOREIGN KEY (EID) REFERENCES Employee(EID)
+		ON DELETE SET NULL
+		ON UPDATE SET NULL,
+	CONSTRAINT Check_total
+		CHECK(total >= 0)
+);
+
+--Author Table
+CREATE TABLE Author(
+	AID INT Identity(1,1),
+	first_name VARCHAR(15),
+	last_name VARCHAR(15),
+	SID INT,
+	PRIMARY KEY (AID),
+	CONSTRAINT FK_SID_Author
+	FOREIGN KEY (SID) REFERENCES Section(SID)
+		ON DELETE SET NULL
+		ON UPDATE SET NULL
+);
+
+--Book Table
+CREATE TABLE Book(
+	ISBN VARCHAR(13),
+	AID INT,
+	title VARCHAR(50),
+	price DECIMAL(15,2),
+	number_of_copies INT,
+	PRIMARY KEY (ISBN),
+	CONSTRAINT FK_AID_Book
+	FOREIGN KEY (AID) REFERENCES Author(AID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	CONSTRAINT Check_price
+		CHECK(price >= 0),
+	CONSTRAINT Check_number_of_copires
+		CHECK(number_of_copies >= 0)
+);
+
+--Book_Copy Table
+CREATE TABLE Book_Copy(
+	ISBN VARCHAR(13),
+	library_book_id INT,
+	PRIMARY KEY (ISBN, library_book_id),
+	CONSTRAINT FK_ISBN_Book_Copy
+	FOREIGN KEY (ISBN) REFERENCES Book(ISBN)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
---
+++ SQL Code/Library_Views_Functions_Procedures_Triggers.sql
@@ -0,0 +1,85 @@
+﻿go
+CREATE VIEW Section_Authors AS
+	SELECT s.genre, a.first_name, a.last_name
+	FROM Section s
+	JOIN Author a ON s.SID = a.SID;
+go
+
+SELECT * FROM Section_Authors ORDER BY genre;
+
+go
+CREATE VIEW Average_Price_For_Author AS
+	SELECT a.first_name + ' ' + a.last_name AS Author_Name,
+	AVG(b.price) AS Average_Book_Price
+	FROM Author a
+	JOIN Book B ON a.AID = b.AID
+	GROUP BY a.first_name, a.last_name;
+go
+
+SELECT * FROM Average_Price_For_Author;
+
+go
+CREATE FUNCTION dbo.Author_Price (@first VARCHAR(15), @last VARCHAR(15))
+RETURNS Decimal(15,2)
+AS
+BEGIN
+DECLARE @avg_price DECIMAL(15,2);
+	SELECT @avg_price = Average_Book_Price
+	FROM Average_Price_For_Author
+	WHERE Author_Name = @first + ' ' + @last
+	RETURN @avg_price;
+END;
+go
+
+SELECT dbo.Author_Price('Terry', 'Pratchett') AS Average_Price;
+
+go
+CREATE PROCEDURE Offboarding
+	@EID INT
+AS
+BEGIN
+	--If Employee Exists
+	IF EXISTS (SELECT 1 FROM Employee WHERE EID = @EID)
+	BEGIN
+		DELETE FROM Employee WHERE EID = @EID
+	END
+	ELSE
+		PRINT 'Employee with specified EID does not exist';
+END;
+go
+
+EXEC Offboarding 33;
+SELECT * FROM Employee WHERE EID = 30;
+
+go
+CREATE TRIGGER updateBookCopies
+ON Book_Copy
+AFTER INSERT, DELETE, UPDATE
+AS 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Book_Copy WHERE ISBN IN 
+        (SELECT DISTINCT ISBN FROM inserted
+        UNION
+        SELECT DISTINCT ISBN FROM deleted))
+    BEGIN
+        RAISERROR('ERROR: The library book ID doesnt exist for the given ISBN', 16, 1)
+        ROLLBACK TRANSACTION;
+    END
+    ELSE
+	    UPDATE b
+	    SET b.number_of_copies = bc.copy_count
+		    FROM Book b
+		    JOIN (
+            SELECT ISBN, COUNT(*) AS copy_count
+            FROM Book_Copy
+            GROUP BY ISBN
+        ) bc ON b.ISBN = bc.ISBN
+        WHERE b.ISBN IN (SELECT DISTINCT ISBN FROM inserted
+                          UNION
+                          SELECT DISTINCT ISBN FROM deleted);
+END;
+go
+
+SELECT number_of_copies FROM Book WHERE ISBN = 9780199650726;
+SELECT * FROM Book_Copy WHERE ISBN = 9780199650726;
+DELETE FROM Book_Copy WHERE ISBN = 9780199650726 AND library_book_id = 10;
